a59d8e8c73b5e296e1f5c9118c266c37b1c9a753
Anil Sachin <anil.pappaka@gmail.com>
2016-08-24 15:07:04 +0530

    JC Declaration

--- app/src/main/java/com/sgp/anil/sharevia/MainActivity.java
+++ app/src/main/java/com/sgp/anil/sharevia/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     String s1="Anil Mohan";
+    String s2="JC";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
